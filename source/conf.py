866ae97f8bf75a2b500d87319d301b9c4548b86a
Michael <iamwuming@163.com>
2024-02-03 13:24:42 +0800

    use sphinx_rtd_theme theme.

--- source/conf.py
+++ source/conf.py
@@ -23,5 +23,8 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 html_static_path = ['_static']
