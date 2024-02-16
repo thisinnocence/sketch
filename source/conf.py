596089e0358f02c05d555a383a3f9181cab824c3
Michael <iamwuming@163.com>
2024-02-16 10:36:16 +0800

    modify project name.

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'wm-sketch'
+project = 'wu-sketch'
 copyright = '2024, Michael-Wu'
 author = 'Michael-Wu'
 
--- source/index.rst
+++ source/index.rst
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to wm-sketch's documentation!
+Michael Wu's sketch
 =====================================
 
 .. toctree::
