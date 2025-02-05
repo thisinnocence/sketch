848407ff3371ec59ca1c3f3cff7c8ca8b3cc638b
Michael <iamwuming@163.com>
2025-02-06 00:32:07 +0800

    fix chinese space proc.

--- source/_extensions/chinese_space.py
+++ source/_extensions/chinese_space.py
@@ -1,4 +1,3 @@
-import os
 from docutils.nodes import NodeVisitor, Text, TextElement, literal_block
 
 def setup(app):
@@ -7,31 +6,31 @@ def setup(app):
 def process_chinese_para(app, doctree, docname):
     doctree.walk(ParaVisitor(doctree))
 
-# 只替换连个中文字符中间的空格
-def mix_replace(text: str) -> str:
-    new_text = text
-    pos = 0
-    while True:
-        pos = new_text.find(os.linesep, pos)
-        if pos < 0:
-            break
-        if pos - 1 < 0:
-            pos = pos + 1
+def _is_asiic_end(text): return bytes(text[-1], 'utf-8')[0] < 128
+
+def _this_is_asiic(text): return bytes(text[0], 'utf-8')[0] < 128
+
+def _tran_chinese_text(text):
+    secs=text.split('\n')
+
+    out = ''
+    last_is_asiic = False
+    for sec in secs:
+        if not sec:
             continue
-        if pos + 1 > len(new_text) - 1:
-            break
-        before = new_text[pos - 1]
-        after = new_text[pos + 1]
-        if len(before.encode()) > 1 and len(after.encode()) > 1:
-            new_text = new_text[:pos] + new_text[(pos + 1):]
-        else:
-            pos = pos + 1
-    return new_text
+
+        if last_is_asiic and _this_is_asiic(sec):
+            out += ' '
+
+        out += sec 
+
+        last_is_asiic = _is_asiic_end(sec)
+
+    return out
 
 class ParaVisitor(NodeVisitor):
     def dispatch_visit(self, node):
         if isinstance(node, TextElement) and not isinstance(node, literal_block):
             for i in range(len(node.children)):
                 if type(node[i]) == Text:
-                    # node[i] = Text(node[i].astext().replace('\r', '').replace('\n', ''))
-                    node[i] = Text(mix_replace(node[i].astext()))
+                    node[i] = Text(_tran_chinese_text(node[i].astext()))
